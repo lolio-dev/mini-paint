168bcde8f65556065f145442c960fad8f5495dc5
Elie Pernet <elie.pernet@orange.fr>
2021-10-15 22:15:52 +0200

    1.0.0

--- mini_paint.py
+++ mini_paint.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from sys import argv
 
 if len(argv) != 2:
@@ -5,38 +6,64 @@ if len(argv) != 2:
     exit()
 
 
-def get_operations():
-    try:
-        with open(argv[1], 'r') as f:
-            canvas_operation = f.readline().split()
-
-            canvas_width = int(canvas_operation[0])
-            canvas_height = int(canvas_operation[1])
-            canvas_char = canvas_operation[2]
-
-            if not 0 < canvas_width <= 300 or not 0 < canvas_height <= 300 or len(canvas_char) != 1:
-                print('Error: Incorrect operation file')
+# get data from .txt files
+try:
+    with open(argv[1], 'r') as f:
+        canvas_operation = f.readline().split()
+        canvas_width = int(canvas_operation[0])
+        canvas_height = int(canvas_operation[1])
+        canvas_char = canvas_operation[2]
+        if not 0 < canvas_width <= 300 or not 0 < canvas_height <= 300 or len(canvas_char) != 1:
+            print('Error: Incorrect operation file')
+            exit()
+        canvas_arguments = (canvas_width, canvas_height, canvas_char)
+        operations = []
+        for i in f.readlines():
+            i = i.replace('\n', '')
+            operations_arguments = i.split()
+            if not operations_arguments[0] in ('r', 'R') or int(operations_arguments[3]) > canvas_arguments[0] \
+                    or int(operations_arguments[4]) > canvas_arguments[1] or len(operations_arguments[5]) != 1:
+                print('Error: Error: Operation file corrupted')
                 exit()
+            operations.append(i)
+except FileNotFoundError:
+    print('Error: Error: Operation file corrupted')
+    exit()
 
-            canvas_arguments = (canvas_width, canvas_height, canvas_char)
-            operations = []
+# Define canvas
+canvas = [list(canvas_char * canvas_width) for _ in range(canvas_height)]
 
-            for i in f.readlines():
-                i = i.replace('\n', '')
-                operations_arguments = i.split()
+# Draw shapes
+for operation in operations:
+    operation = operation.split()
 
-                if not operations_arguments[0] in ('r', 'R') or int(operations_arguments[3]) > canvas_arguments[0] or int(operations_arguments[4]) > canvas_arguments[1] or len(operations_arguments[5]) != 1:
-                    print('Error: Incorrect operation file')
-                    exit()
+    begin_X = int(operation[1])
+    begin_Y = int(operation[2])
 
-                operations.append(i)
+    shape_type = operation[0]
+    width = int(operation[3])
+    height = int(operation[4])
+    character = operation[5]
 
-            return canvas_operation, operations
+    try:
+        if shape_type == 'R':
+            for j in range(begin_Y - 1, begin_Y + height, 1):
+                canvas[j][begin_X] = operation[5]
 
-    except FileNotFoundError:
+                for i in range(begin_X - 1, begin_X + width, 1):
+                    canvas[j][i] = operation[5]
+        elif shape_type == 'r':
+            for i in range(begin_X, begin_X + width, 1):
+                canvas[begin_Y][i] = character
+                canvas[begin_Y + height - 1][i] = character
+
+            for i in range(begin_Y, begin_Y + height, 1):
+                canvas[i][begin_X] = character
+                canvas[i][begin_X + width] = character
+    except IndexError:
         print('Error: Error: Operation file corrupted')
         exit()
 
-
-print(get_operations())
-
+# Display Canvas
+for i in canvas:
+    print(''.join(i))
